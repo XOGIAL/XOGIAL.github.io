d29d270163fe9aba62a6240c5d3ec7a38d0c626f
Riksen Setiawan <riksensetiawan@gmail.com>
2021-10-18 21:41:42 +0700

    fixjs7

--- JS/Script.js
+++ JS/Script.js
@@ -33,15 +33,26 @@ function educationbtn() {
 
   function contactmebtn() {
     var x = document.getElementById("contactmedropdowncontainerid");
-    if (x.style.height < "5px") {
-     
+    if (x.style.height < "10px") {
+      x.style.transition ="";
       var thisheight = x.scrollHeight;
       x.style.display = "flex"
       x.style.height = thisheight +'px';
       x.style.transition =".5s"
-    } else {
-      
+    } else if (x.style.height>"10px"){
+      forceclose();
       x.style.transition =".5s"
-      x.style.height= "0px"
+      x.style.height ="0 px"
     }
   }
+
+  function forceclose()
+  {
+    var x = document.getElementById("contactmedropdowncontainerid");
+    x.style.height = "0px"
+  }
+
+
+  document.getElementById("educationbtnid").addEventListener("click", function() {
+    educationbtn();
+  });
